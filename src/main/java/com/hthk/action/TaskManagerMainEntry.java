a923d86a2336a8aefdd2ec544450948c9a6962cb
rockchen <rochchen@htsc.com>
2023-11-16 16:29:08 +0800

    update

--- src/main/java/com/hthk/action/TaskManagerMainEntry.java
+++ src/main/java/com/hthk/action/TaskManagerMainEntry.java
@@ -1,18 +1,45 @@
 package com.hthk.action;
 
+import com.hthk.fintech.config.AppConfig;
+import com.hthk.fintech.config.FintechStaticData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * @Author: Rock CHEN
  * @Date: 2023/11/16 15:29
  */
+@SpringBootApplication(scanBasePackages = "com.hthk")
 public class TaskManagerMainEntry {
 
-    public static void main(String[] args) {
+    private final static Logger logger = LoggerFactory.getLogger(TaskManagerMainEntry.class);
+
+    private void start(String[] args) {
+
+        String configPath =
+                new StringBuilder("spring.config.location=")
+                        .append("classpath:/")
+                        .append(FintechStaticData.DEFAULT_CLASS_PATH)
+                        .append("/")
+                        .toString();
+
+        String appName = null;
+        try {
+            SpringApplicationBuilder builder = new SpringApplicationBuilder(TaskManagerMainEntry.class)
+                    .properties(configPath);
+            builder.run(args);
+            logger.info("{} kick-off", builder.context().getBean(AppConfig.class).getAppName());
+        } catch (Throwable e) {
+            logger.error("{} exit with Exception:\r\n{}", appName, e.getMessage(), e);
+            System.exit(1);
+        }
 
     }
 
-
-    public void start() {
-
+    public static void main(String[] args) {
+        new TaskManagerMainEntry().start(args);
     }
 
 }
--- src/main/java/com/hthk/service/impl/TaskManagerServiceImpl.java
+++ src/main/java/com/hthk/service/impl/TaskManagerServiceImpl.java
@@ -1,19 +1,47 @@
 package com.hthk.service.impl;
 
+import com.hthk.action.TaskManagerMainEntry;
+import com.hthk.common.utils.CustomStringUtils;
+import com.hthk.common.utils.LocalDateTimeUtils;
+import com.hthk.fintech.config.AppConfig;
+import com.hthk.fintech.model.task.ITask;
+import com.hthk.fintech.service.basic.AbstractService;
 import com.hthk.service.TaskManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.hthk.fintech.config.FintechStaticData.LOG_DEFAULT;
+import static com.hthk.fintech.config.FintechStaticData.LOG_WRAP;
 
 /**
  * @Author: Rock CHEN
  * @Date: 2023/11/16 15:29
  */
 @Component
-public class TaskManagerServiceImpl implements TaskManagerService {
+public class TaskManagerServiceImpl
 
+        extends AbstractService implements TaskManagerService {
+
+    private final static Logger logger = LoggerFactory.getLogger(TaskManagerServiceImpl.class);
+
+    @PostConstruct
     @Override
     public LocalDateTime start() {
+
+        LocalDateTime currentTime = LocalDateTimeUtils.getNow();
+        List<ITask> taskList = loadAllTask(appConfig);
+        String logStr = CustomStringUtils.buildWrapStr(taskList);
+        logger.info(LOG_WRAP, "task list", logStr);
+
+        return currentTime;
+    }
+
+    private List<ITask> loadAllTask(AppConfig appConfig) {
         return null;
     }
 
